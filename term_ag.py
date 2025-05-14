e96e35e7ee10c0bcc3d2b42ed10b3b1ee52da581
picon <s.wielgosz@noxgle.com>
2025-05-14 10:50:40 +0200

    Update welcome message and error handling in term_agent for consistency

--- term_ag.py
+++ term_ag.py
@@ -354,25 +354,23 @@ def main():
     agent = term_agent()
     agent.console.print(PIPBOY_ASCII)
     ai_status,mode_owner,ai_model = agent.check_ai_online()
-    agent.console.print("\nWelcome, Vault Dweller, to the Vault-Tec 3000.\n")
+    agent.console.print("\nWelcome, Vault Dweller, to the Vault 3000.\n")
     
     if ai_status:
-        agent.console.print(f"""Fallout-inspired AI Agent ({ai_model}) is online.\n""")
+        agent.console.print(f"""AgentAI ({ai_model}) is online. What can I do for you today?\n""")
     else:
         agent.console.print("[red]Fallout-inspired AI Agent is offline.[/]\n")
         agent.console.print("[red]Please check your API key and network connection.[/]\n")
         sys.exit(1)
-
-    agent.console.print("[Vault-Tec] What can I do for you today?\n")
     
     try:
         user_goal = agent.console.input("> ")
         #user_goal = "sprawdz ilsoć wolengo miejsca na dysku"
     except EOFError:
-        agent.console.print("\n[red][Vault-Tec] EOFError: Unexpected end of file.[/]")
+        agent.console.print("\n[red][Vault 3000] EOFError: Unexpected end of file.[/]")
         sys.exit(1)
     except KeyboardInterrupt:
-        agent.console.print("\n[red][Vault-Tec] Sttopeed by user.[/]")
+        agent.console.print("\n[red][Vault 3000] Sttopeed by user.[/]")
         sys.exit(1)
     if len(sys.argv) == 2:
         remote = sys.argv[1]
@@ -380,19 +378,19 @@ def main():
         host = remote.split('@')[1] if '@' in remote else remote
         agent.ssh_connection = True  # Ustaw tryb zdalny
         agent.remote_host = remote   # Przechowuj host do użycia w execute_remote
-        agent.console.print(f"[Vault-Tec] AI agent started with goal: {user_goal} on {remote}.\n")
+        agent.console.print(f"VaultAI AI agent started with goal: {user_goal} on {remote}.\n")
     else:
         remote = None
         user = None
         host = None
         agent.ssh_connection = False
         agent.remote_host = None
-        agent.console.print(f"[Vault-Tec] AI agent started with goal: {user_goal}")
+        agent.console.print(f"VaultAI AI agent started with goal: {user_goal}")
     runner = VaultAIAgentRunner(agent, user_goal, user=user, host=host)
     try:
         runner.run()
     except KeyboardInterrupt:
-        agent.console.print("[red]Agent przerwany przez użytkownika.[/]")
+        agent.console.print("[red][Vault 3000] Agent przerwany przez użytkownika.[/]")
         # Możesz dodać podsumowanie lub zapis stanu
 
 if __name__ == "__main__":
